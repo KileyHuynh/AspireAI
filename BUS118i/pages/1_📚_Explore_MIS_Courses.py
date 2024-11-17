b3ed9ed1287ec927773095e1e3dcb69a1bc81cb3
Kiley Huynh <kiley.huynh@sjsu.edu>
2024-11-17 00:50:56 -0800

    drft

--- BUS118i/pages/1_📚_Explore_MIS_Courses.py
+++ BUS118i/pages/1_📚_Explore_MIS_Courses.py
@@ -20,7 +20,7 @@ def get_completion(system_prompt, user_prompt, model="gpt-3.5-turbo"):
 st.header(":blue[SJSU MIS Courses] 📚", divider="rainbow")
 st.markdown(":violet[**AspireAI**] is here to help you understand how your courses translate into career opportunities, giving you the insights you need to plan your future with confidence. These course descriptions are available on the [SJSU website](https://www.sjsu.edu/isystems/resume_course_descriptions.php), and you're welcome to include them on your resume!")
 st.subheader("👩🏻‍💻 :violet[Explore MIS Courses]")
-st.markdown("*Select a course number(s) to get the description.*")
+#st.markdown("*Select a course number(s) to get the description.*")
 
 courses = {
     "BUS4 092 - Introduction to Business Programming": "Employed Python programming (loops, branching, file access, and error handling) and teamwork skills to develop a notebook-based project to access and analyze data from the web.",
